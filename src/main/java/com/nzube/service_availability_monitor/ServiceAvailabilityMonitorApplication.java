94c969aeafa6ae5409f9fbbfb7fca9fe64a166c5
Adaobi-Chuks <chuksaginamada@gmail.com>
2026-01-27 06:10:26 +0100

    Chore: Setup a springboot project

---
+++ src/main/java/com/nzube/service_availability_monitor/ServiceAvailabilityMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.nzube.service_availability_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceAvailabilityMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceAvailabilityMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nzube/service_availability_monitor/ServiceAvailabilityMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nzube.service_availability_monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceAvailabilityMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
