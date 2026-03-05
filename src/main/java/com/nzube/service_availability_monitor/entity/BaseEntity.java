c377b7907abbac83536f33b0b1e0cfd37dda364a
Adaobi-Chuks <chuksaginamada@gmail.com>
2026-03-05 09:34:56 +0100

    Feat: Base entity setup

---
+++ src/main/java/com/nzube/service_availability_monitor/entity/BaseEntity.java
@@ -0,0 +1,30 @@
+package com.nzube.service_availability_monitor.entity;
+
+import java.time.Instant;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@MappedSuperclass
+@Getter
+@Setter
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    private Instant updatedAt;
+
+}
