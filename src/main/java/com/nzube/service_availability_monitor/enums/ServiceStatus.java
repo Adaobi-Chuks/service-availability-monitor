069c8398a56ed1fe00612e2da558fcd7fa44b14c
Adaobi-Chuks <chuksaginamada@gmail.com>
2026-03-20 10:10:05 +0100

    feat: add service status and type enums for monitoredservice entity

---
+++ src/main/java/com/nzube/service_availability_monitor/enums/ServiceStatus.java
@@ -0,0 +1,5 @@
+package com.nzube.service_availability_monitor.enums;
+
+public enum ServiceStatus {
+    UP, DOWN, UNKNOWN
+}
---
+++ src/main/java/com/nzube/service_availability_monitor/enums/ServiceType.java
@@ -0,0 +1,5 @@
+package com.nzube.service_availability_monitor.enums;
+
+public enum ServiceType {
+    HTTP, TCP
+}
