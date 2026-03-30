273d00b33f6637b9f9937e0a47703ed38c124b0e
Adaobi-Chuks <chuksaginamada@gmail.com>
2026-03-30 10:45:22 +0100

    config: configure swagger

---
+++ src/main/java/com/nzube/service_availability_monitor/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.nzube.service_availability_monitor.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Service Availability Monitor API")
+                        .description("API for monitoring HTTP and TCP service availability")
+                        .version("1.0.0")
+                )
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Auth"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Auth", new SecurityScheme()
+                                .name("Bearer Auth")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                        )
+                );
+    }
+}
