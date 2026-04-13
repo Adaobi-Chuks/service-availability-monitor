797476aceaee45d526e128749c51ebf599f263e7
Adaobi-Chuks <chuksaginamada@gmail.com>
2026-04-13 19:20:44 +0100

    feat

--- src/main/java/com/nzube/service_availability_monitor/ServiceAvailabilityMonitorApplication.java
+++ src/main/java/com/nzube/service_availability_monitor/ServiceAvailabilityMonitorApplication.java
@@ -2,13 +2,15 @@ package com.nzube.service_availability_monitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mail.MailProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.nzube.service_availability_monitor.config.property.JwtProperties;
+import com.nzube.service_availability_monitor.config.property.RabbitMQProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({JwtProperties.class})
+@EnableConfigurationProperties({JwtProperties.class, RabbitMQProperties.class, MailProperties.class})
 @EnableScheduling
 public class ServiceAvailabilityMonitorApplication {
 
